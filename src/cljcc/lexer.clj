cc3d1fa53ece0056a7bcef1dd199a366089dfecd
rufo <rufo@rufoa.com>
2016-01-31 05:58:04 +0000

    Swap multimethods for protocol+records

--- src/cljcc/lexer.clj
+++ src/cljcc/lexer.clj
@@ -18,10 +18,6 @@
 			base (if (zero? newlines) char 1)]
 		(assoc position :line (+ line newlines) :char (+ base offset))))
 
-
-(defmulti ^:private matcher        "Is the given string an instance of the token?" (comp type :pattern))
-(defmulti ^:private prefix-matcher "Could the given string go on to become an instance of the token, given a suitable suffix?" (comp type :pattern))
-
 (defn- ^:testable Pattern->RegExp
 	"Convert java.util.regex.Pattern to dk.brics.automaton.RegExp so we can manipulate it as a DFA"
 	^dk.brics.automaton.RegExp [re]
@@ -47,51 +43,69 @@
 			(s/replace #"(?<!\\)(\\\\)*\u0034" "$1\\\u0034") ; double quotes need to be escaped
 			(RegExp.))))
 
-(defmethod matcher java.lang.String
-	[{:keys [pattern ci] :as token}]
-	(fn [string]
-		(when
-			(and
-				(>= (count string) (count pattern))
-				(let [string' (if ci (s/lower-case string)  string)
-				      pattern' (if ci (s/lower-case pattern) pattern)]
-					(= (subs string' 0 (count pattern')) pattern')))
-			{:consumed (subs string 0 (count pattern))
-			 :token token})))
+(defprotocol Token
+	(matcher [_]
+		"Is the given string an instance of the token?")
+	(prefix-matcher [_]
+		"Could the given string go on to become an instance of the token, given a suitable suffix?"))
 
-(defmethod prefix-matcher java.lang.String
-	[{:keys [pattern ci]}]
-	(fn [string]
-		(and
-			(<= (count string) (count pattern))
-			(let [string (if ci (s/lower-case string)  string)
-			      pattern (if ci (s/lower-case pattern) pattern)]
-				(= string (subs pattern 0 (count string)))))))
-
-; you can't memoize with defmethod
-(.addMethod ^clojure.lang.MultiFn prefix-matcher java.util.regex.Pattern
-	(memoize (fn [{:keys [pattern]}]
-		(let [automaton (.toAutomaton (Pattern->RegExp pattern))]
-			(doseq [^State state (.getStates automaton)]
-				(.setAccept state true))
-			(.restoreInvariant automaton)
-			(.setDeterministic automaton true)
-			(let [run-automaton (RunAutomaton. automaton)]
-				(fn [string]
-					(.run run-automaton string)))))))
-
-(defmethod matcher java.util.regex.Pattern
-	[{:keys [pattern valuation-fn] :as token}]
-	(let [pattern (re-pattern (str "^" pattern))] ; anchor at start with ^
+(defrecord StringToken [pattern ci]
+	Token
+	(matcher [token]
 		(fn [string]
-			(when-let [match (re-find pattern string)]
-				(if valuation-fn
+			(when
+				(and
+					(>= (count string) (count pattern))
+					(let [string' (if ci (s/lower-case string)  string)
+					      pattern' (if ci (s/lower-case pattern) pattern)]
+						(= (subs string' 0 (count pattern')) pattern')))
+				{:consumed (subs string 0 (count pattern))
+				 :token token})))
+	(prefix-matcher [token]
+		(fn [string]
+			(and
+				(<= (count string) (count pattern))
+				(let [string (if ci (s/lower-case string)  string)
+				      pattern (if ci (s/lower-case pattern) pattern)]
+					(= string (subs pattern 0 (count string))))))))
+
+(defrecord RegexToken [pattern valuation-fn anchored-pattern ^RunAutomaton prefix-run-automaton]
+	Token
+	(matcher [token]
+		(fn [string]
+			(when-let [match (re-find anchored-pattern string)]
+				(if (:valuation-fn token)
 					{:consumed (if (string? match) match (first match))
-					 :value (valuation-fn match)
+					 :value ((:valuation-fn token) match)
 					 :token token}
 					{:consumed (if (string? match) match (first match))
-					 :token token})))))
+					 :token token}))))
+	(prefix-matcher [token]
+		(fn [string]
+			(.run prefix-run-automaton string))))
 
+(defn- regex-token
+	[token]
+	(let
+		[anchored-pattern (re-pattern (str "^" (:pattern token))) ; anchor at start with ^
+		 automaton (.toAutomaton (Pattern->RegExp (:pattern token)))]
+		(doseq [^State state (.getStates automaton)]
+			(.setAccept state true))
+		(.restoreInvariant automaton)
+		(.setDeterministic automaton true)
+		(let [prefix-run-automaton (RunAutomaton. automaton)]
+			(map->RegexToken
+				(assoc token
+					:anchored-pattern anchored-pattern
+					:prefix-run-automaton prefix-run-automaton)))))
+
+(defn- make-token
+	[token]
+	(cond
+		(string? (:pattern token))
+			(map->StringToken token)
+		(instance? java.util.regex.Pattern (:pattern token))
+			(regex-token token)))
 
 (defn- buffer-enough
 	"Pull on the input just enough that we can correctly determine the next token"
@@ -109,33 +123,34 @@
 	[tokens]
 	(if (some #(= (:name %) :$) tokens)
 		(throw+ {:type ::reserved-symbol :message "Token name :$ is reserved for internal use"})
-		(letfn
-			[(lazy-lex
-				[buffer input position]
-				(let [[buffer input] (buffer-enough tokens buffer input)]
-					(if (empty? buffer)
-						(cons {:token {:name :$} :position position} nil) ; end sentinel
-						(let [cands
-							(->> tokens
-								(map #((matcher %) buffer) ,,,)
-								(filter #(and (not (nil? %)) (not-empty (:consumed %))) ,,,))] ; must match and must also consume input
-							(if (empty? cands)
-								(throw+ {:type ::unexpected-input :message (str "Unexpected input or possible premature end of input at " position)})
-								(let [best-match
-									(last (sort-by
-										(fn [{:keys [consumed token]}] [(count consumed) (:priority token 0)]) ; scoring fn: longest match, then :priority
-										cands))]
-									(lazy-seq
-										(cons
-											(assoc best-match :position position)
-											(lazy-lex
-												(subs buffer (count (:consumed best-match)))
-												input
-												(update-position position (:consumed best-match)))))))))))]
-			(fn [input]
-				(map
-					#(->
-						(assoc % :token-name (get-in % [:token :name]))
-						(dissoc % :token))
-					(filter (comp not :ignore :token)
-						(lazy-lex "" input (Position. 1 1))))))))
+		(let [tokens (map make-token tokens)]
+			(letfn
+				[(lazy-lex
+					[buffer input position]
+					(let [[buffer input] (buffer-enough tokens buffer input)]
+						(if (empty? buffer)
+							(cons {:token {:name :$} :position position} nil) ; end sentinel
+							(let [cands
+								(->> tokens
+									(map #((matcher %) buffer) ,,,)
+									(filter #(and (not (nil? %)) (not-empty (:consumed %))) ,,,))] ; must match and must also consume input
+								(if (empty? cands)
+									(throw+ {:type ::unexpected-input :message (str "Unexpected input or possible premature end of input at " position)})
+									(let [best-match
+										(last (sort-by
+											(fn [{:keys [consumed token]}] [(count consumed) (:priority token 0)]) ; scoring fn: longest match, then :priority
+											cands))]
+										(lazy-seq
+											(cons
+												(assoc best-match :position position)
+												(lazy-lex
+													(subs buffer (count (:consumed best-match)))
+													input
+													(update-position position (:consumed best-match)))))))))))]
+				(fn [input]
+					(map
+						#(->
+							(assoc % :token-name (get-in % [:token :name]))
+							(dissoc % :token))
+						(filter (comp not :ignore :token)
+							(lazy-lex "" input (Position. 1 1)))))))))
